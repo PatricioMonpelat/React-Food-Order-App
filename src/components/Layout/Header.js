855f82f24f3618779868807f9f617036ac5d3aa5
Patricio Monpelat <patricio.monpelat@gmail.com>
2022-08-30 13:54:24 -0300

    update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,15 +2,16 @@ import React, { Fragment } from "react";
 import mealsImage from "../../assets/1.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-import arg from '../../assets/arg.png';
+import palta from '../../assets/arg.png';
 
 const Header = (props) => {
   return (
     <Fragment>
     
       <header className={classes.header}>
-        <h1>HomeLand Food </h1>
-        <img src={arg} alt='argentina food' className={classes.arg}/>
+        {/* <h1>HomeLand Food </h1> */}
+        <img src={palta} alt='argentina food' className={classes.palta}/>
+        <h1>Argentina National Food</h1>
         <HeaderCartButton onClick={props.onshowCart}/>
       </header>
       <div className={classes["main-image"]}>
